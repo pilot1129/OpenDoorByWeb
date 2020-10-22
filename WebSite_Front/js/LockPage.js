b16061a1149f08a56fad48fa59d8db1783589007
kim ki hwan <37990051+pilot1129@users.noreply.github.com>
2020-10-22 20:26:25 +0900

    khuhub to github(Frontend)
    2019.9~2019.12

---
+++ WebSite_Front/js/LockPage.js
@@ -0,0 +1,15 @@
+element = document.getElementById("hey");
+
+element.addEventListener("click", function(e) {
+  e.preventDefault;
+  
+  // -> removing the class
+  element.classList.remove("bounce");
+  
+  // -> triggering reflow /* The actual magic */
+  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+  element.offsetWidth = element.offsetWidth;
+  
+  // -> and re-adding the class
+  element.classList.add("bounce");
+}, false);
